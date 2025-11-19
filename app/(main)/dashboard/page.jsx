e36516a4c293a49923091171e36d558079a8e1f8
Apurv Kumar <kumarapurv2610@gmail.com>
2025-11-19 16:24:40 +0530

    fix: dashboard

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,19 +1,22 @@
 import { getIndustryInsights } from "@/actions/dashboard";
+import DashboardView from "./_components/dashboard-view";
 import { getUserOnboardingStatus } from "@/actions/user";
 import { redirect } from "next/navigation";
-import DashboardView from "./_components/dashboard-view";
 
-const IndustryInsightsPage = async() => {
-    const { isOnboarded } = await getUserOnboardingStatus();
-    const insights = await getIndustryInsights();
-        if (!isOnboarded) {
-            redirect("/onboarding");
-    }
-    
-    return( <div className="container mx-auto">
-        <DashboardView insights={insights}/>
+export default async function DashboardPage() {
+  const { isOnboarded } = await getUserOnboardingStatus();
+
+  // If not onboarded, redirect to onboarding page
+  // Skip this check if already on the onboarding page
+  if (!isOnboarded) {
+    redirect("/onboarding");
+  }
+
+  const insights = await getIndustryInsights();
+
+  return (
+    <div className="container mx-auto">
+      <DashboardView insights={insights} />
     </div>
-    );
-};
-
-export default IndustryInsightsPage;
+  );
+}
