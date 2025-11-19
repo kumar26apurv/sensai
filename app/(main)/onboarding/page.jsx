ab59780bbe37016ba9c75b5aa4e698e4558ed75d
Apurv Kumar <kumarapurv2610@gmail.com>
2025-11-19 15:38:39 +0530

    fix: onboarding

--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -1,20 +1,19 @@
-import { getUserOnboardingStatus } from "@/actions/user";
-import { industries } from "@/data/industries";
 import { redirect } from "next/navigation";
+import { industries } from "@/data/industries";
 import OnboardingForm from "./_components/onboarding-form";
+import { getUserOnboardingStatus } from "@/actions/user";
 
-const OnboardingPage = async() => {
-    //check if user is already onboarded
-    const { isOnboarded } = await getUserOnboardingStatus();
-    if (isOnboarded) {
-        redirect("/dashboard");
-    }
-    return (
-        <main>
-            <OnboardingForm industries={industries } />
-        </main>
-    );
-};
+export default async function OnboardingPage() {
+  // Check if user is already onboarded
+  const { isOnboarded } = await getUserOnboardingStatus();
 
-export default OnboardingPage;
+  if (isOnboarded) {
+    redirect("/dashboard");
+  }
 
+  return (
+    <main>
+      <OnboardingForm industries={industries} />
+    </main>
+  );
+}
